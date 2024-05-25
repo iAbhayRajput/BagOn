9f9e6b11bad6e808b14f6e511255c836459600f9
iAbhayRajput <abhayravirajput@gmail.com>
2024-05-25 16:00:08 +0530

    changed  Config files and OwnerCreation

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
+
+if(process.env.NODE_ENV === "development"){
+router.post("/create", async function (req, res){
+    
+   let owner = await ownerModel.find();
+   if (owner.length > 0) {
+    return res
+    .status(500)
+    .send("You Dont Have Permission To create a new owner");
+   }
+
+   let {fullname, email, password} = req.body;
+
+   let createdOwner =  await ownerModel.create({
+    fullname,
+    email,
+    password,
+   })
+
+   res.status(201).send(createdOwner);
+});
+}
 
 router.get("/", function (req, res){
     res.send("Hey! it's working");
 });
+
 
 module.exports =router ;
